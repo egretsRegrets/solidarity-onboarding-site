5c51bf22a8b70d48a450cc9cf9a33d6111a4c7b4
egretsRegrets <samuel.galen.gardner@gmail.com>
2019-11-10 12:47:24 -0500

    working page and doodle templating

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,42 @@
 const fg = require('fast-glob');
 
-const pages = fg.sync(['assets/pages/*']);
+const pageImages = fg.sync(['assets/pages/*']);
+const doodleArray = [];
+
+const firstPageOffset = 7;
+
+pageImages.forEach((_, pageNumber) => {
+    const doodlePath = 'assets/doodles/page_' + (pageNumber + firstPageOffset).toString() + '/*';
+    doodleArray.push(fg.sync(doodlePath));
+});
+
+const pageAndDoodleTemplates = pageImages.map((pageImageSrc, pageIndex) => {
+    const doodlesForPage = doodleArray[pageIndex].map((doodleSrc) => {
+        return `
+            <li>
+                <img src="${doodleSrc}"/>
+            </li>        
+        `;
+    }).join('');
+    return `
+        <div class="page-image-container">
+            <img src="${pageImageSrc}"/>
+        </div>
+        <div class="doodle-images-container">
+            <ul>
+                ${doodlesForPage}
+            </ul>
+        </div>
+    `;
+});
 
 module.exports = function(eleventyConfig) {
+    // eleventyConfig.addCollection('pages', function(collection) {
+    //     return pages
+    // });
+
     eleventyConfig.addCollection('pages', function(collection) {
-        return pages
+        return pageAndDoodleTemplates;
     });
 
     eleventyConfig.addPassthroughCopy('assets');
